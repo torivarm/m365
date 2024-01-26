106ad762921eb2da75aeffcc8a9d0834d1f5b187
Tor Ivar Melling <torivar.melling@gmail.com>
2024-01-26 11:00:00 +0100

    Create 02-01-MgGraph-CheckList.ps1

---
+++ 02-01-MgGraph-CheckList.ps1
@@ -0,0 +1,13 @@
+$Details = Get-MgContext
+$Scopes = $Details | Select-Object -ExpandProperty Scopes
+$Scopes = $Scopes -join ","
+$OrgName = (Get-MgOrganization).DisplayName
+
+"Microsoft Graph current session details:"
+"---------------------------------------"
+"Tenant Id = $($Details.TenantId)"
+"Client Id = $($Details.ClientId)"
+"Org name  = $OrgName"
+"App Name  = $($Details.AppName)"
+"Account   = $($Details.Account)"
+"Scopes    = $Scopes"
