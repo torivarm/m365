56f25efdf257ef6ba34c94a43341bd3446631dfb
Tor Ivar Melling <tor.i.melling@ntnu.no>
2024-02-06 14:37:55 +0100

    update

---
+++ 03-01-CreateGroups.ps1
@@ -0,0 +1,44 @@
+$TenantID = "06bf9568-f5b8-4a8b-bef5-1f2291b53f76"
+Connect-MgGraph -TenantId $TenantID -Scopes "User.ReadWrite.All", "Group.ReadWrite.All", "Directory.ReadWrite.All", "RoleManagement.ReadWrite.Directory"
+
+# Create groups
+Get-Help New-MgGroup -Online
+
+# Create M365 Group for IT
+
+$departments = @("Sales", "Marketing", "Finance", "Support")
+foreach ($department in $departments) {
+    $membershiprule = "user.department -eq `"$department`""
+    $Params = @{
+        DisplayName = "$department Team"
+        Description = "Group for all $department employees"
+        MailEnabled = $true
+        MailNickname = $department
+        SecurityEnabled = $true
+        GroupTypes = @("Unified", "DynamicMembership")
+        MembershipRule = $membershiprule
+        MembershipRuleProcessingState = "On"
+    }
+
+    New-MgGroup @Params
+}
+
+
+
+
+
+
+$Params = @{
+    DisplayName = "IT Team"
+    Description = "Team for all IT employees"
+    MailEnabled = $true
+    MailNickname = "it"
+    SecurityEnabled = $true
+    GroupTypes = @("Unified", "DynamicMembership")
+    MembershipRule = 'user.department -eq "IT"'
+    MembershipRuleProcessingState = "On"
+    }
+
+New-MgGroup @Params
+
+
